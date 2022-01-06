3df345a5e45ae4ab452b17633307dfb119603b02
Joe Curtis <joecurtis9999@gmail.com>
2022-01-06 11:28:21 +0000

    Added h1 tag to navbar with new fonts

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
+
+
+
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='webvisuals__navbar'>
+            <div className='webvisuals__navbar-links'>
+                <div className='webvisuals__navbar-links_logo'>
+                    <h1>webvisuals</h1>
+                </div>
+            </div>
+            
             
         </div>
     )
