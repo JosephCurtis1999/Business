e86dfe966c883decdc3ca8a4fdb64f1c419c518b
Joe Curtis <joecurtis9999@gmail.com>
2022-02-03 14:38:38 +0000

    media queries for services section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='webvisuals__header section__padding' id='home'>
             <div className='webvisuals__header-content'>
-                <h1>Welcome to webvisuals. We build, deploy and manage websites for your business</h1>
+                <h1>Welcome to webvisuals. We build, deploy and manage websites for your business.</h1>
                 <p>Your website is just as important as your store front or menu. In this day and age, the first impression
                     of your business will be what users encounter on your website. Invest in your business by using our services
                     to build and deploy a modern, interactive website.
